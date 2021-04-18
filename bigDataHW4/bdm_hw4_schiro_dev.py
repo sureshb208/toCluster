e090bb7e6936d18639346a6568912d91e543f6ae
= <fahraynk@gmail.com>
2021-04-17 23:47:51 -0400

    debugging

--- bigDataHW4/bdm_hw4_schiro_dev.py
+++ bigDataHW4/bdm_hw4_schiro_dev.py
@@ -96,7 +96,7 @@ if __name__=='__main__':
     .map(lambda x: (pipe(x, str)[:10], 0))
 
     from pyspark.sql.session import SparkSession
-    patternFile = os.path.join(data, "weekly-patterns-nyc-2019-2020/*")
+    #patternFile = os.path.join(data, "weekly-patterns-nyc-2019-2020/*")
     spark = SparkSession(sc)
     pattern = spark.read.csv(patternFile, multiLine=True, header="True",sep = ",", escape= "\n")
     pattern.filter(pattern['safegraph_place_id'].isin(set4)) \
