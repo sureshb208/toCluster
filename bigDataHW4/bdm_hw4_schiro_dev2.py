5fba66c0934b7f818b3db5521d1765a5b60a2e56
= <fahraynk@gmail.com>
2021-04-18 04:08:07 -0400

    debugging

--- bigDataHW4/bdm_hw4_schiro_dev2.py
+++ bigDataHW4/bdm_hw4_schiro_dev2.py
@@ -116,6 +116,9 @@ if __name__=='__main__':
     ) \
     .select(['safegraph_place_id', 'date_range_start', 'date_range_end', 'visits_by_day']) \
     .rdd.map(lambda x: trnsfm(x)).flatMap(lambda x: x) \
+    .saveAsTextFile("TEST")
+
+    #.saveAsTextFile(os.path.join(root, "results/TEST"))
     #.union(dateData) \
     #.groupByKey().map(lambda x:  (x[0], np.median([i for i in x[1]]), np.std([i for i in x[1]]))) \
-    .saveAsTextFile("TEST")
+    
